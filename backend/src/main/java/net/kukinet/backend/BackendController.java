fbffb5c631b3075f6ba445655b1c0fd9c82426c0
Chen Alkabets <chen.alkabets@nova-lumos.com>
2018-12-01 07:53:55 +0200

    adding url /vars to print envvars

--- backend/src/main/java/net/kukinet/backend/BackendController.java
+++ backend/src/main/java/net/kukinet/backend/BackendController.java
@@ -3,6 +3,8 @@ package net.kukinet.backend;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 /**
  * Created by lms on 30/11/18.
  */
@@ -13,6 +15,17 @@ public class BackendController {
     @RequestMapping("/hello")
     public String sayHello() {
         return "hello from backend.";
+    }
+
+    @RequestMapping("/vars")
+    public String showEnvironmetVars() {
+
+        String reply = "";
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            reply += String.format("%s=%s%n", envName, env.get(envName));
+        }
+        return reply;
     }
 
 }
